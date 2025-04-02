ecebca2776c82d455ec633d95b6b3fe4c1cc4261
Biniam <biniambehailu3@gmail.com>
2025-04-02 06:14:46 +0300

    first commit

---
+++ dashboard.php
@@ -0,0 +1,101 @@
+<?php 
+include 'db.php'; 
+session_start();
+
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') { 
+        $firstName = $_POST['firstName']; 
+        $lastName = $_POST['lastName']; 
+        $username = $_POST['username']; 
+        $password = $_POST['password']; 
+        $password = password_hash($password, PASSWORD_DEFAULT);
+    
+        $stmt = $pdo->prepare("INSERT INTO users (firstName, lastName, username, 
+    password) VALUES (?, ?, ?, ?)"); 
+        $stmt->execute([$firstName, $lastName, $username, $password]); 
+        $success = "Data Inserted Successfully";    
+    } 
+
+    $stmt = $pdo->query("SELECT * FROM users"); 
+    $users = $stmt->fetchAll(); 
+?> 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .container{
+            margin: 1rem;
+            padding: 1rem;
+            border: 1px solid #333;
+            border-radius: 10px;
+        }
+        .user{
+            float: right;
+        }
+        .alert p{
+            color: green;
+
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <p class="user">Hello: <?php if(isset($_SESSION['firstName']) && isset($_SESSION['lastName'])){
+            echo $_SESSION['firstName'] . " ".$_SESSION['lastName'];
+        } ?>
+    <a href="logout.php">Logout</a>    
+    </p>
+        <br>
+        <div class="content">
+            <h3>Welcome to the Dashboard</h3>
+        </div>
+        <br>
+
+        <?php 
+            if(isset($success)){
+                ?>
+            <div class="alert">
+                <p><?php echo $success; ?></p>
+            </div>
+
+            <?php
+            }
+        ?><br>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <label for="">First Name</label><br>
+            <input type="text" name="firstName"><br>
+            <label for="">Last Name</label><br>
+            <input type="text" name="lastName"><br>
+            <label for="">Username</label><br>
+            <input type="text" name="username"><br>
+            <label for="">Password</label><br>
+            <input type="text" name="password"><br><br>
+            <input type="submit">
+        </form>
+        <br>
+        <table> 
+            <thead>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Username</th>
+                <th>Status</th>
+            </thead>
+        <?php foreach ($users as $user): ?> 
+            <tbody> 
+                <td><?php echo $user['firstName']; ?></td>
+                <td><?php echo $user['lastName']; ?> </td>
+                <td><?php echo $user['username']; ?> </td>
+                <td><?=  $user['status'] == 1 ? 'Active' : 'Not Active'; ?> </td>
+                
+                
+            </tbody> 
+            <?php endforeach; ?> 
+        </table> 
+    </div>
+    
+</body>
+</html>
+
---
+++ index.php
@@ -0,0 +1,86 @@
+<?php
+include "db.php";
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = htmlspecialchars($_POST['username']);
+    $password = htmlspecialchars($_POST['password']);
+
+    $stmt = $pdo->prepare("SELECT * FROM users WHERE username = :username ");
+    $stmt->bindParam(':username', $username);
+    $stmt->execute();
+
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+    var_dump($user);
+
+    if ($user && password_verify($password, $user['password'])) {
+        $_SESSION['id'] = $user['id'];
+        $_SESSION['firstName'] = $user['firstName'];
+        $_SESSION['lastName'] = $user['lastName'];
+
+        $last_login = date('d-m-Y H:i:s:a');
+        
+        $stmt2 = $pdo->prepare("INSERT INTO users (last_login_date) VALUES (?)"); 
+        $stmt2->execute([$last_login]); 
+
+
+        header("location:dashboard.php");
+         
+    } else {
+        echo "Invalid username or password.";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background-color:rgb(228, 228, 228);
+            height: 100vh;
+            background: url("bg.png") no-repeat;
+            background-size: cover;
+        }
+        .login{
+            background-color: #fff;
+            padding: 1rem;
+            width: 300px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            border-radius: 10px;
+        }
+        input{
+            border: 1px solid #000;
+            border-radius: 5px;
+            height: 30px;
+            width: 250px;
+        }
+        .btn{
+            color: #fff;
+            background-color: #000;
+        }
+    </style>
+</head>
+<body>
+<div class="login">
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+        <h3>Login</h3> 
+        <label for="username">Username</label><br>
+        <input type="text" name="username"><br>
+        <label for="password">Password</label><br>
+        <input type="password" name="password"><br><br>
+        <input type="submit" value="Login" class="btn">
+    </form>
+</div>
+
+    
+</body>
+</html>
